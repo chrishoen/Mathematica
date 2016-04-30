34d828649863ae60e7b440cc3a185c7e8e7988cf
chris.hoen <chris.hoen@verizon.net>
2016-04-30 15:19:59 -0400

    more

--- Lib/MathTensor.m
+++ Lib/MathTensor.m
@@ -19,7 +19,8 @@ Return[LT];]
 
 \[Gamma][v_]:= 1/Sqrt[1-v^2];
 \[Gamma][{v1_,v2_,v2_}]:= 1/Sqrt[1-(v1^2 + v2^2  v3^2) ];
-MyNorm3[a3_] := Sqrt[a3[[1]]^2 + a3[[2]]^2 + a3[[3]]^2]
+Length3[a_]:= Sqrt[a[[1]]^2 + a[[2]]^2 + a[[3]]^2]
+Normal3[a_]:= a/Sqrt[a[[1]]^2 + a[[2]]^2 + a[[3]]^2]
 Velocity4From3[v3_]:=\[Gamma][MyNorm3[v3]] Prepend[v3,1]
 Velocity3From4[v4_]:=Rest[v4]/First[v4]
 DistSQFrom4[a_,b_] := (a[[2]] - b[[2]])^2 + (a[[3]] - b[[3]])^2 +(a[[4]] - b[[4]])^2 ;
@@ -27,6 +28,7 @@ DistFrom4[a_,b_] := Sqrt[(a[[2]] - b[[2]])^2 + (a[[3]] - b[[3]])^2 +(a[[4]] - b[
 DistSQ3[a_,b_] := (a[[1]] - b[[1]])^2 + (a[[2]] - b[[2]])^2 + (a[[3]] - b[[3]])^2
 DistSQ4[a_,b_] := (a[[2]] - b[[2]])^2 + (a[[3]] - b[[3]])^2 +(a[[4]] - b[[4]])^2 
 Position3From4[A4_]:=Return[{A4[[2]],A4[[3]],A4[[4]]}];
+Vector4[T_,X3_]:=Return[{T,X3[[1]],X3[[2]],X3[[3]]}];
 Coulomb3[R3o_,R3s_]:=Module[{Numer,Denom},
 Numer=(R3o-R3s);
 Denom=(((R3o[[1]] - R3s[[1]])^2 + (R3o[[2]] - R3s[[2]])^2 + (R3o[[3]] - R3s[[3]])^2)^(3/2));
