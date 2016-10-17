06ecd1f8192cf29713b47b1117975f8ee57c1886
chrishoen <chris.hoen@red-inc.us>
2016-10-17 15:25:29 -0400

    more

--- Lib/Rotations.m
+++ Lib/Rotations.m
@@ -5,6 +5,10 @@ RotateX[t_] := (M1 = {{1,0,0},{0,Cos[t],-Sin[t]},{0,Sin[t],Cos[t]}};Return[M1]);
 RotateY[t_] := (M1 = {{Cos[t],0,Sin[t]},{0,1,0},{-Sin[t],0,Cos[t]}};Return[M1]);
 RotateZ[t_] := (M1 = {{Cos[t],-Sin[t],0},{Sin[t],Cos[t],0},{0,0,1}};Return[M1]);
 
+RotateCSX[cos_,sin_] := (M1 = {{1,0,0},{0,cos,-sin},{0,sin,cos}};Return[M1]);
+RotateCSY[cos_,sin_] := (M1 = {{cos,0,sin},{0,1,0},{-sin,0,cos}};Return[M1]);
+RotateCSZ[cos_,sin_] := (M1 = {{cos,-sin,0},{sin,cos,0},{0,0,1}};Return[M1]);
+
 RotateXInv[t_] := (M1 = {{1,0,0},{0,Cos[t],Sin[t]},{0,-Sin[t],Cos[t]}};Return[M1]);
 RotateYInv[t_] := (M1 = {{Cos[t],0,-Sin[t]},{0,1,0},{Sin[t],0,Cos[t]}};Return[M1]);
 RotateZInv[t_] := (M1 = {{Cos[t],Sin[t],0},{-Sin[t],Cos[t],0},{0,0,1}};Return[M1]);
